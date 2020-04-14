e4398f6ba66d3175532fa8dc299aa178a707e329
ltarnowb <ltarnowb@gmail.com>
2020-04-14 17:47:06 -0400

    HTML CSS JS Added

---
+++ script.js
@@ -0,0 +1,16 @@
+if('geolocation' in navigator){
+    console.log("This works")
+}else{
+    console.log("This doesn't work.")
+}
+
+function getLoc(){
+
+}
+
+function success(postion){
+    checkLoc(position.)
+
+    
+}
+
