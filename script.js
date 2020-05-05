505157f9d4e01befa18da1580ca5c11273c57ff1
ltarnowb <ltarnowb@gmail.com>
2020-05-05 18:53:45 -0400

    Add content
    Added the Video content.

--- script.js
+++ script.js
@@ -4,39 +4,41 @@
 var homeNav = document.getElementById("home");
 var perNav = document.getElementById("per");
 var locNav = document.getElementById("loc");
-var abtNav = document.getElementById("abt");
+//var abtNav = document.getElementById("abt");
 
 
 //content div
 var abtDiv = document.getElementById("aboutDiv");
 var perDiv = document.getElementById("perDiv");
 var locDiv = document.getElementById("prezDiv");
-var vidDiv = document.getElementById("vidDiv");
+//var vidDiv = document.getElementById("vidDiv");
 
 var vid = document.getElementById("vid");
-var wel = document.getElementById("wel");
+// var wel = document.getElementById("wel");
 
-var welcome = "https://www.youtube.com/embed/lojuls2PmYs"; 
-wel.src = welcome;
+// var welcome = "https://www.youtube.com/embed/lojuls2PmYs"; 
+// wel.src = welcome;
+
+var locTBD = "notavail.jpg";
 
 
-//0: Crash Course 1: SNL 2: Sleep No More
-var locations = ["https://www.youtube.com/embed/sNWrOuwzax8","https://www.youtube.com/embed/2f7tmS1oig4","https://www.youtube.com/embed/k12NZLh_Xvg"];
+//0: Crash Course 1: SNL 2: Sleep No More 3: tech quickie
+var locations = ["https://www.youtube.com/embed/W567e9o8ljQ","https://www.youtube.com/embed/2f7tmS1oig4","https://www.youtube.com/embed/W567e9o8ljQ","https://www.youtube.com/embed/-pUz9fIwvxk"];
 
 var cLong;
 
 var cLat;
 
-//0: home 1: baird 2: pier 
-var latArr = [43.000437, 42.907082, 42.954447];
+//0: home 1: baird 2: pier 3: Dubai
+var latArr = [43.000437, 42.907082, 42.954447, 25.099438];
 
-var longArr = [-78.781884, -78.902606, -78.832048];
+var longArr = [-78.781884, -78.902606, -78.832048, 55.256597];
 
 
 
-var delLat = .000940;
+var delLat = .000697;
 
-var delLong = .000697;
+var delLong = .000940;
 
 var geo;
 var watchID;
@@ -46,15 +48,15 @@ window.onload = geoUpdate();
 //Navigation Bar Interface
 function homeNv(event){
     console.log("home");
-    vidDiv.style.display = "block"; 
+    //vidDiv.style.display = "none"; 
     perDiv.style.display = "none";
     prezDiv.style.display = "none"; 
-    abtDiv.style.display = "none";
+    abtDiv.style.display = "inline-block";
 }
 
 function perNv(event){
     console.log("performance");
-    vidDiv.style.display = "none"; 
+    //vidDiv.style.display = "none"; 
     perDiv.style.display = "block";
     prezDiv.style.display = "none"; 
     abtDiv.style.display = "none";
@@ -62,19 +64,19 @@ function perNv(event){
 
 function locNv(event){
     console.log("location");
-    vidDiv.style.display = "none"; 
+    //vidDiv.style.display = "none"; 
     perDiv.style.display = "none";
     prezDiv.style.display = "block"; 
     abtDiv.style.display = "none";
 }
 
-function abtNv(event){
-    console.log("about");
-    vidDiv.style.display = "none";
-    perDiv.style.display = "none"; 
-    prezDiv.style.display = "none"; 
-    abtDiv.style.display = "block";
-}
+// function abtNv(event){
+//     console.log("about");
+//     vidDiv.style.display = "none";
+//     perDiv.style.display = "none"; 
+//     prezDiv.style.display = "none"; 
+//     abtDiv.style.display = "inline-block";
+// }
 
 homeNav.addEventListener("click", homeNv);
 
@@ -82,15 +84,9 @@ perNav.addEventListener("click", perNv);
 
 locNav.addEventListener("click", locNv);
 
-abtNav.addEventListener("click", abtNv);
+//abtNav.addEventListener("click", abtNv);
 
-//Geolocation Software
-if('geolocation' in navigator){
-    console.log("This works")
-}else{
-    console.log("This doesn't work.")
-}
-
+//Geolocation Script
 
 function getLocation() {
   if (navigator.geolocation) {
@@ -112,7 +108,7 @@ function showPosition(position) {
 function testLocation(){
     if(getLocation()){
         var location = navigator.geolocation.getCurrentPosition(showPosition, errorLocation);
-        console.log();
+        console.log("check");
     }
 }
 
@@ -129,7 +125,7 @@ function locationCheck(){
                 console.log("Location: " + i);
                 break;
         }else{
-            vid.src = "welcome";
+            vid.src = locTBD;
         }
     }
 }
